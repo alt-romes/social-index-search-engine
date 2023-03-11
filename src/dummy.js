2fc6df729d5ab3c6e8656c25149accf8ae28e421
tometom <tombsantos@gmail.com>
2023-03-11 10:23:54 +0000

    Fixed json parsing, meili search from ids and query now working

--- src/dummy.js
+++ src/dummy.js
@@ -1,24 +1,30 @@
 const { MeiliSearch } = require('meilisearch')
-const  express  = require('express')
+const express = require('express')
 const dummy = require('./dummy.json')
 const app = express()
+app.use(express.json())
+
 const client = new MeiliSearch({ host: 'http://localhost:7700' })
 client.index('pagecontents').addDocuments(dummy)
     .then((res) => { console.log(res) })
+client.index('pagecontents')
+    .updateFilterableAttributes([
+        'id',
+    ])
 
-
-
+app.get("/", function (req, res) {
+    console.log("ok")
+    res.send("ok")
+})
 app.get('/search', function (req, res) {
+    console.log("ok")
     console.log(req.body)
-    let json = JSON.parse(req.body.trim())
-    let query = json.query
-    let ids = json.ids
-    let ids_as_string = ids.map(id =>{id.toString()+","})
-    ids_as_string = "[" + ids_as_string.slice(0,-1) + "]"
-    client.index('pagecontents').search(query,{filter: 'id in '+ ids_as_string}).then((res) => console.log(res))
+    let query = req.body.query
+    let ids = req.body.ids
+    client.index('pagecontents').search(query, { filter: 'id IN ' + ids }).then((result) => res.send(result))
 })
 app.listen(3000, () => {
     console.log('Server started on port 3000');
-  });
+});
 
 
