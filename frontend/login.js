15922146335e34e9b4f4cee3775c883da8b04108
tometom <tombsantos@gmail.com>
2023-03-11 23:36:42 +0000

    Fixed cookies

--- frontend/login.js
+++ frontend/login.js
@@ -1,23 +1,47 @@
-var auth_token = "";
 
-function login(url,username, password){
-    putData(url+"?username=" +username+"&password=" + password,{}).then(data =>{auth_token = data;return data;})
+function login(url, username, password) {
+    putData(url + "?username=" + username + "&password=" + password, {}).then(data => {return data; })
 }
 
 
 function putData(url = "", data = {}) {
     // Default options are marked with *
     return fetch(url, {
-      method: "PUT", // *GET, POST, PUT, DELETE, etc.
-      mode: "cors", // no-cors, *cors, same-origin
-      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: "same-origin", // include, *same-origin, omit
-      headers: {
-        "Content-Type": "application/json",
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      redirect: "follow", // manual, *follow, error
-      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify(data), // body data type must match "Content-Type" header
-    }).then(r => r.json());
-  }
+        method: "PUT", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+            "Content-Type": "application/json",
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+}
+function postData(url = "", data = {}) {
+    let cookies =document.cookie.split("; ")
+    let auth_cookie = ""
+    cookies.forEach(el =>{let s = el.split("="); if(s[0] == "authcookie"){auth_cookie = s[1]}})
+
+    // Default options are marked with *
+    return fetch(url, {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+            "Content-Type": "application/json",
+            "Authentication": `Bearer ${auth_cookie}`
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data), // body data type must match "Content-Type" header
+    })
+        ;
+}
+function signUp(username, email, password) {
+    postData("/user?username=" + username + "&email=" + email + "&password=" + password).then(login("/login", username, password))
+}
--- frontend/search.js
+++ frontend/search.js
@@ -28,23 +28,4 @@ function addBookmark(url){
 }
 
 
-// Example POST method implementation:
-function postData(url = "", data = {}) {
-  console.log(auth_token["jwt"])
-  // Default options are marked with *
-  return fetch(url, {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    mode: "cors", // no-cors, *cors, same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit
-    headers: {
-      "Content-Type": "application/json",
-      "Authentication": `Bearer ${auth_token["jwt"]}`
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: "follow", // manual, *follow, error
-    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data), // body data type must match "Content-Type" header
-  });
-}
 
