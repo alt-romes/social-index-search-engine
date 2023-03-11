e88900936ca3faf55cdab61b6a45f32c3f90c9c3
tometom <tombsantos@gmail.com>
2023-03-11 11:31:57 +0000

    Dummy removed body in GET and put in params

--- src/dummy.js
+++ src/dummy.js
@@ -12,19 +12,41 @@ client.index('pagecontents')
         'id',
     ])
 
-app.get("/", function (req, res) {
-    console.log("ok")
-    res.send("ok")
-})
+   // fetch(url).then(resp => {
+
+   // })
+
+
+
 app.get('/search', function (req, res) {
-    console.log("ok")
-    console.log(req.body)
-    let query = req.body.query
-    let ids = req.body.ids
+    let query = req.query.query
+    let ids = req.query.ids
     client.index('pagecontents').search(query, { filter: 'id IN ' + ids }).then((result) => res.send(result))
+})
+app.post("/bookmark", function (req, res) {
+    //Receives url in req.body
+    let url = req.body.url
+    let userID = req.body.userID
+    let pagecontents;
+    getJSON(url, (code, response) => {
+        if (code == null) {
+            pagecontents = response;
+        }
+        else{
+            console.log(code)
+            return;
+        }
+    })
+    if(pagecontents != null){
+        console.log(pagecontents)
+    }
+    //Go to DB, get follows of userID
+
 })
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
+
+
 
 
