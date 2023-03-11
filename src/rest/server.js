8e7771a14344cb664ee822821e726ac0108df1d8
Manded <49417189+Manded@users.noreply.github.com>
2023-03-11 19:10:21 +0000

    TokenDB done

--- src/rest/server.js
+++ src/rest/server.js
@@ -1,5 +1,6 @@
 const path = require('node:path');
 const jwt = require("jsonwebtoken");
+const TOKEN_KEY = "SampleText"
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 //meilisearch
 const { MeiliSearch } = require('meilisearch')
@@ -37,11 +38,19 @@ app.put('/login', (req, res) => {
     let username = req.query.username
     let password = req.query.password
     db.all('SELECT * FROM users WHERE username =?', username, (error1, qres1) => {
-        if (qres.values == 0) {
+        if (qres1.values.length == 0) {
             res.send('No user called ' + username + ' exists')
         } else {
             db.all('SELECT * FROM ? WHERE password=?', qres, password, (error2, qres2) => {
-                if (qres2) {
+                if (qres2.values.length == 0) {
+                    const token = jwt.sign(
+                        { user_id: qres1.uid },
+                        TOKEN_KEY,
+                        {
+                            expiresIn: "2h",
+                        }
+                    );
+                    db.run('INSERT INTO tokens (jwt, uid) VALUES (?,?)', token, qres1.uid)
                     res.send({ "userid": userid })
                 } else {
                     res.send('Wrong password')
@@ -61,9 +70,9 @@ app.post('/bookmark', (req, res) => {
         let title = htmlParser("head title").text()
         let description = htmlParser("meta[name='description']").attr().content
         console.log("Title: " + title + "\nDesc: " + description)
-        if (title == null || description == null) { res.send("Invalid website");return; }
+        if (title == null || description == null) { res.send("Invalid website"); return; }
         db.all('SELECT * FROM bookmarks WHERE url =?', link, (error, qres) => {
-            if(error != null){
+            if (error != null) {
                 console.log(error)
             }
             if (qres.length == 0) {
@@ -72,18 +81,18 @@ app.post('/bookmark', (req, res) => {
                         res.send("Insert failed")
                         return;
                     }
-                    db.all("SELECT last_insert_rowid() FROM bookmarks", (row_err,row_id) =>{addContentLine(row_id[0]["last_insert_rowid()"],link,title,description,res)})
+                    db.all("SELECT last_insert_rowid() FROM bookmarks", (row_err, row_id) => { addContentLine(row_id[0]["last_insert_rowid()"], link, title, description, res) })
                 })
             }
             else {
-                addContentLine(qres[0].bid,link,title,description,res)
+                addContentLine(qres[0].bid, link, title, description, res)
             }
-            db.all('SELECT * FROM userbookmarks WHERE bid=?',last_row,(err,rows)=>{
-                if(err != null){
+            db.all('SELECT * FROM userbookmarks WHERE bid=?', last_row, (err, rows) => {
+                if (err != null) {
                     res.send("userbookmarks failed")
                 }
-                if(rows.length == 0){
-                    db.run('INSERT INTO userbookmarks (bid,uid) VALUES (?,?)',last_row,userID)
+                if (rows.length == 0) {
+                    db.run('INSERT INTO userbookmarks (bid,uid) VALUES (?,?)', last_row, userID)
                 }
                 console.log(rows)
             })
@@ -96,11 +105,11 @@ app.post('/follow', (req, res) => {
     user = req.query.user
     targetuser = req.query.targetuser
     db.all('SELECT * FROM users WHERE uid=?', user, (err, qres1) => {
-        if (qres.values == 0) {
+        if (qres1.values.length == 0) {
             res.send('You do not exist')
         } else {
             db.all('SELECT * FROM users WHERE uid=?', user, (err, qres2) => {
-                if (qres.values == 0) {
+                if (qres2.values.length == 0) {
                     res.send('Target user does not exist')
                 } else {
                     db.run('INSERT INTO followers (uidfollower, uidfollowed) VALUES  (?, ?)', user, targetuser)
@@ -108,23 +117,21 @@ app.post('/follow', (req, res) => {
             })
         }
     })
-    //Add target user to followlist in DB
-    //if no target user then return 404
     res.send('followed user! ' + targetuser)
 })
 
 app.get('/search', (req, res) => {
     let query = req.query.query
     let user = req.query.user
-    db.all('SELECT uidfollowed FROM followers WHERE uidfollower =?',user,(err1,rows1) =>{
+    db.all('SELECT uidfollowed FROM followers WHERE uidfollower =?', user, (err1, rows1) => {
         rows1.push(user)
         let id_string = "("
-        rows1.forEach((el) =>{id_string = id_string.concat(el + ",")})
+        rows1.forEach((el) => { id_string = id_string.concat(el + ",") })
         id_string = id_string.concat(")")
-        id_string = id_string.replace(",)" , ")")
-        db.all('SELECT bid FROM userbookmarks WHERE uid IN '+id_string,(err2,rows2) =>{
+        id_string = id_string.replace(",)", ")")
+        db.all('SELECT bid FROM userbookmarks WHERE uid IN ' + id_string, (err2, rows2) => {
             rows2 = rows2.map(el => el.bid)
-            rows2 = rows2.filter(el => el!=null)
+            rows2 = rows2.filter(el => el != null)
             client.index('pagecontents').search(query).then((result) => res.send(result))
             res.sendFile(path.join(__dirname, '../../frontend/search.html'));
         })
@@ -136,7 +143,7 @@ app.listen(localport, () => {
     console.log(`Example app listening on port ${localport}`)
 })
 
-function addContentLine(last_row,link,title,description,res){
+function addContentLine(last_row, link, title, description, res) {
     client.index('pagecontents').addDocuments([{
         id: last_row,
         url: link,
