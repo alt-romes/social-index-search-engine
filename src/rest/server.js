035280391362467d7acb59742eb718565ede9382
Manded <49417189+Manded@users.noreply.github.com>
2023-03-11 12:15:14 +0000

    db start

--- src/rest/server.js
+++ src/rest/server.js
@@ -1,36 +1,51 @@
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
-
+//meilisearch
+const { MeiliSearch } = require('meilisearch')
+//test
+const dummy = require('../dummy.json')
 const express = require('express')
 const app = express()
 const localport = 3000
-const meiliport = 7700
+app.use(express.json())
+
+const client = new MeiliSearch({ host: 'http://localhost:7700' })
+client.index('pagecontents').addDocuments(dummy)
+    .then((res) => { console.log(res) })
+client.index('pagecontents')
+    .updateFilterableAttributes([
+        'id',
+    ])
 
 app.post('/bookmark', (req, res) => {
     res.send('Hello World!')
-    fetch('http://localhost:' + meiliport + '/bookmark', {
-        method: 'POST',
-        body: JSON.stringify({
-            title: 'foo',
-            body: 'bar',
-            userId: 1,
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    })
-        .then((response) => response.json())
-        .then((json) => console.log(json))
-        .catch(error => {
-            console.log(error)
-        })
+    let user = req.query.user
+    let link = req.body.link
+    //parse HTML
+    let pagebody = req.body
+    client.index('pagecontents').addDocuments(dummy)
+        .then((res) => { console.log(res) })
+    client.index('pagecontents')
+        .updateFilterableAttributes([
+            'id',
+        ])
+
 })
 
 app.post('/follow', (req, res) => {
-    res.send('followed user!')
+    user = req.query.user
+    targetuser = req.query.targetuser
+
+    //Add target user to followlist in DB
+    //if no target user then return 404
+    res.send('followed user! ' + targetuser)
+
 })
 
 app.get('/search', (req, res) => {
-    res.send('Hello World!')
+    let query = req.query.query
+    let user = req.query.user
+    let ids = req.query.id //temporary
+    client.index('pagecontents').search(query, { filter: 'id IN ' + ids }).then((result) => res.send(result))
 })
 
 app.listen(localport, () => {
